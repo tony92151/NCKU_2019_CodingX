a5ff302d509ce9e1041b804a192e8eb0bbb7538e
tony <tony92151@gmail.com>
2019-07-25 19:34:40 +0800

    target up

--- 02_java/project/game/app/src/main/java/com/example/hitthebullseye/MainActivity.java
+++ 02_java/project/game/app/src/main/java/com/example/hitthebullseye/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.hitthebullseye;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+
+        btn = (Button)findViewById(R.id.playbt);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity_s.class);
+                //intent.putExtra("accDF",accDataF);
+                startActivity(intent);
+            }
+        });
     }
 }
--- 02_java/project/game/app/src/main/java/com/example/hitthebullseye/MainActivity_s.java
+++ 02_java/project/game/app/src/main/java/com/example/hitthebullseye/MainActivity_s.java
@@ -59,18 +59,18 @@ public class MainActivity_s extends AppCompatActivity implements SensorEventList
         }
 
 
-        buttonBack = (Button)findViewById(R.id.backbt);
+        buttonBack = (Button)findViewById(R.id.backbutton);
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(MainActivity_s.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
         imgv = (ImageView)findViewById(R.id.imgview);
 
-        eye = (ImageView)findViewById(R.id.eye);
+        //eye = (ImageView)findViewById(R.id.eye);
         imgv.setX((int)(accDataF[0]*100));
         imgv.setY((int)(accDataF[1]*100));
         System.out.println("move");
@@ -109,11 +109,11 @@ public class MainActivity_s extends AppCompatActivity implements SensorEventList
                             accDataF[1] = y_filter.update(accData[1],"Y");
                             accDataF[2] = z_filter.update(accData[2],"Z");
                             count++;
-                            imgv.setX((int)y_filter.getdir()*250-2000);
-                            eye.setX((int)y_filter.getdir()*250-2000);
+                            imgv.setX((int)y_filter.getdir()*250-1000);
+                            //eye.setX((int)y_filter.getdir()*250-1000);
                             System.out.println("X dir = " +(x_filter.getdir()*10+200));
-                            imgv.setY((int)x_filter.getdir()*250-2500);
-                            eye.setY((int)x_filter.getdir()*250-2500);
+                            imgv.setY((int)x_filter.getdir()*250-1800);
+                            //eye.setY((int)x_filter.getdir()*250-2000);
                             System.out.println("y dir = " +(y_filter.getdir()*10+400));
                             //System.out.println("move");
                         }
